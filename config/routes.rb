e785d172c9537449ee6f6b7707486222a15fb6ce
takareinhard <hokke.takara@gmail.com>
2019-08-10 13:01:24 +0900

    insert Rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'groups#index'
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
