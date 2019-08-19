442bb9d8274bae98cc45add913760c5741030eb3
takareinhard <hokke.takara@gmail.com>
2019-08-19 20:33:40 +0900

    Java script for beauty style

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,8 +5,8 @@ var selected_list = $(".chat-group-users");
 
 function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
-               <p class="chat-group-user__name">${user.name}</p>
-               <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>`
   search_list.append(html);
 }
@@ -21,9 +21,9 @@ function appendErrMsgToHTML(msg) {
 function appendList(userName, userId) {
   
   var html = `<div class='chat-group-user'>
-               <input name='group[user_ids][]' type='hidden' value='${userId}'>
-               <p class='chat-group-user__name'>${userName}</p>
-               <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                <p class='chat-group-user__name'>${userName}</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
               </div>`
   selected_list.append(html);
 }
