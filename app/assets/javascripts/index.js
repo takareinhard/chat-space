8b40919b65a364960e2dc62cec29da3704ad1ab9
takareinhard <hokke.takara@gmail.com>
2019-08-20 01:13:07 +0900

    except current user get data

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -28,9 +28,10 @@ function appendList(userName, userId) {
   selected_list.append(html);
 }
 
-
-  $("#user-search-field").on("keyup", function() {
+  $("#user-search-field").on("keyup", function(e) {
+    e.preventDefault();
     var input = $("#user-search-field").val();
+
 
     $.ajax({
       type: 'GET',
@@ -42,8 +43,8 @@ function appendList(userName, userId) {
     .done(function(users) {
       $('#user-search-result').empty();
       if (users.length !== 0) {
-        users.forEach(function(user){
-        appendUser(user);
+      users.forEach(function(user){
+      appendUser(user);
       });
        }
       else {
