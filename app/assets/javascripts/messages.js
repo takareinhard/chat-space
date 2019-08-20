f79503b55c6994e49dc0c58b4b98779c7b4074c5
takareinhard <hokke.takara@gmail.com>
2019-08-20 20:49:02 +0900

    edit message.js for reload

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -48,4 +48,24 @@ $(function(){
       alert('エラー');
     })
   })
+
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id = message.id
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: api/messages,
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log('success');
+  })
+  .fail(function() {
+    console.log('error');
+    });
+  };
 });
