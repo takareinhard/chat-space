2ff62fdc80a26a5e99fbc63b0c28812f66a2a4c0
takareinhard <hokke.takara@gmail.com>
2019-08-21 20:51:25 +0900

    changed files for perfect

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,57 +1,56 @@
-$(function(){
+$(function() {
 
-  var buildMessageHTML = function(message) {
+  function buildHTML(message) {
     if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
-    var html = `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                    ${message.user_name}
-                    </div>
-                    <div class="upper-message__date">
-                    ${message.date}
-                    </div>
-                  </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                  ${content}
-                  </p>
-                  ${img}
-                 </div>
-                </div>`
-            } else if (message.content) {
-     var html = '<div class="message" data-id=' + message.id + '>' +
-                '<div class="upper-message">' +
-                  '<div class="upper-message__user-name">' +
-                    message.user_name +
-                  '</div>' +
-                  '<div class="upper-message__date">' +
-                    message.created_at +
-                  '</div>' +
-                '</div>' +
-                '<div class="lower-message">' +
-                  '<p class="lower-message__content">' +
-                    message.content +
-                  '</p>' +
-                '</div>' +
-              '</div>'
-            } else if (message.image.url) {
       var html = '<div class="message" data-id=' + message.id + '>' +
-                '<div class="upper-message">' +
-                  '<div class="upper-message__user-name">' +
-                    message.user_name +
-                  '</div>' +
-                  '<div class="upper-message__date">' +
-                    message.created_at +
-                  '</div>' +
-                '</div>' +
-                '<div class="lower-message">' +
-                  '<img src="' + message.image.url + '" class="lower-message__image" >' +
-                '</div>' +
-              '</div>'
-            };
-            return html;
-          };
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.created_at +
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+          '<img src="' + message.image.url + '" class="lower-message__image" >' +
+        '</div>' +
+      '</div>'
+    } else if (message.content) {
+      var html = '<div class="message" data-id=' + message.id + '>' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.created_at +
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+        '</div>' +
+      '</div>'
+    } else if (message.image.url) {
+      var html = '<div class="message" data-id=' + message.id + '>' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.created_at +
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<img src="' + message.image.url + '" class="lower-message__image" >' +
+        '</div>' +
+      '</div>'
+    };
+    return html;
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -69,36 +68,37 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      var html = buildMessageHTML(message);
+      var html = buildHTML(message);
       $('.messages').append(html)
       $("form")[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('エラー');
-    })
-  })
+    });
+  });
 
 var reloadMessages = function() {
-  last_message_id = message.id
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  last_message_id = $('.message:last').data('id'); 
   $.ajax({
-    url: api/messages,
+    url: "api/messages" ,
     type: 'get',
     dataType: 'json',
     data: {id: last_message_id}
   })
   .done(function(messages) {
-    //追加するHTMLの入れ物を作る
     var insertHTML = '';
-    messages.forEach(function(insertHTML){
-    var html = buildMessageHTML(message);
-    $('.messages').append(html)
+    messages.forEach(function(message){
+    insertHTML = buildHTML(message);
+    $('.messages').append(insertHTML);
+  })
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
   })
   .fail(function() {
-    console.log('error');
-        });
-    });
-  };
-setInterval(reloadMessages, 5000);
+    alert('error');
+  });
+ }
+};
+  setInterval(reloadMessages, 5000);
 });
