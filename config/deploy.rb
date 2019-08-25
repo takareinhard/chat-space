b7bd85d614b8a740838ccd131dbdb1d6cecca821
takareinhard <hokke.takara@gmail.com>
2019-08-25 18:33:56 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -53,10 +61,4 @@ end
 # Capistrano の場合は以下を設定する
 set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
 
